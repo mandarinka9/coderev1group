48159df2a0ad4b5d1930cfb2776f20653e055137
mandarinka9 <145179517+mandarinka9@users.noreply.github.com>
2025-05-23 14:06:14 +0500

    Update SecondClass.cs

--- 1/SecondClass.cs
+++ 1/SecondClass.cs
@@ -2,28 +2,28 @@ using System;
 using System.Linq;
 
 namespace _1labasharpcoderev {
-    public class SecondClass : FirstClass {
-        public DateTime CreatedAt { get; }
+  public class SecondClass : FirstClass {
+    public DateTime CreatedAt { get; }
 
-        public SecondClass(string text) : base(text) {
-            CreatedAt = DateTime.Now;
-        }
-
-        public int Length => Text.Length;
-
-        public string Vverh() {
-            return Text.ToUpper();
-        }
-
-        public int CountGlas() {
-            const string vowels = "aeiouyаеёиоуыэюя";
-            return Text.ToLower()
-                      .Where(c => char.IsLetter(c) && vowels.Contains(c))
-                      .Count();
-        }
-
-        public override string ToString() {
-            return $"{base.ToString()}, Дата создания: {CreatedAt}";
-        }
+    public SecondClass(string text) : base(text) {
+      CreatedAt = DateTime.Now;
     }
+
+    public int Length => Text.Length;
+
+    public string Vverh() {
+      return Text.ToUpper();
+    }
+
+    public int CountGlas() {
+      const string vowels = "aeiouyаеёиоуыэюя";
+      return Text.ToLower()
+                .Where(c => char.IsLetter(c) && vowels.Contains(c))
+                .Count();
+    }
+
+    public override string ToString() {
+      return $"{base.ToString()}, Дата создания: {CreatedAt}";
+    }
+  }
 }
